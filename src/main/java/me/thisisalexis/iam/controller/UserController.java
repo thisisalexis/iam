ddb7ae67b8abe855b53372f1ae43331680572138
Alexis Bravo <alexis.bravo@globant.com>
2022-06-29 17:15:31 -0400

    feat: getUserById mocked method added to UsersController

--- src/main/java/me/thisisalexis/iam/controller/UserController.java
+++ src/main/java/me/thisisalexis/iam/controller/UserController.java
@@ -1,23 +1,39 @@
 package me.thisisalexis.iam.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import me.thisisalexis.iam.dto.UserRecord;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
+//TODO Add Swagger documentation
+//TODO Add security
 public class UserController {
+
+    //TODO Delete this
+    private static final UserRecord mockUser() {
+        return new UserRecord("thisisalexis", "Alexis", "Bravo", "alexis.ve@gmail.com", LocalDate.now(), LocalDateTime.now(), LocalDateTime.now());
+    }
 
     @GetMapping
     public ResponseEntity<?> getUsers() {
-        final UserRecord userRecord = new UserRecord("thisisalexis", "Alexis","Bravo", "alexis.ve@gmail.com", LocalDate.now(), LocalDateTime.now(), LocalDateTime.now());
-        return ResponseEntity.of(Optional.of(userRecord));
+        log.info("Querying all users");
+        return ResponseEntity.ok(List.of(mockUser()));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable String userId) {
+        log.info("Querying user {}", userId);
+        return ResponseEntity.ok(mockUser());
     }
 
 }
