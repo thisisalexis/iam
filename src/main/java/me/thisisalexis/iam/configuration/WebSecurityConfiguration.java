d931b01eb45b4903cb659a0592218442b0d1c858
Alexis Bravo <alexis.bravo@globant.com>
2022-06-29 17:25:15 -0400

    feat: SpringsecurityWebConfigurerAdapter configuration bean added to project

---
+++ src/main/java/me/thisisalexis/iam/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package me.thisisalexis.iam.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/actuator", "/users").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    /*
+    //TODO Evaluar qué hace esto en Spring Security.
+       @see Tomado desde https://spring.io/guides/gs/securing-web/
+    	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	**/
+
+}
